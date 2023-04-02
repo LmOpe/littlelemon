dc6a2df44e145a366bf42cdff063c9168a75df0a
LmOpe <lawalmuhammed44@gmail.com>
2023-04-02 14:08:40 +0100

    Set up user registration system

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
     'djoser',
 ]
 
@@ -133,5 +134,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    }
 DJOSER={"USER_ID_FIELD":"username"}
